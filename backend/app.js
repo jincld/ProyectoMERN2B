7a467b96063ca2f23aaf2753f9ff3aae3df10b90
Exequiel <exequiel.miranda245@gmail.com>
2025-03-25 07:37:21 -0600

    registro de empleados

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,16 @@ import customersRoutes from "./src/routes/customers.js";
 import employeeRoutes from "./src/routes/employees.js";
 import branchesRoutes from "./src/routes/branches.js";
 import reviewRoutes from "./src/routes/reviews.js";
+import registerEmployesRoutes from "./src/routes/registerEmployees.js";
+import cookieParser from "cookie-parser";
 
 // Creo una constante que es igual a la libreria que importé
 const app = express();
 
 //Que acepte datos en json
 app.use(express.json());
+//Que acepte cookies en postman
+app.use(cookieParser());
 
 // Definir las rutas de las funciones que tendrá la página web
 app.use("/api/products", productsRoutes);
@@ -18,8 +22,7 @@ app.use("/api/customers", customersRoutes);
 app.use("/api/employee", employeeRoutes);
 app.use("/api/branches", branchesRoutes);
 app.use("/api/reviews", reviewRoutes);
-
-// 
+app.use("/api/registerEmployees", registerEmployesRoutes);
 
 // Exporto la constante para poder usar express en otros archivos
 export default app;
--- backend/src/config.js
+++ backend/src/config.js
@@ -11,4 +11,8 @@ export const config = {
   server: {
     port: process.env.PORT,
   },
+  JWT: {
+    secret: process.env.JWT_SECRET,
+    expiresIn: process.env.JWT_EXPIRES
+  }
 };
